54f281bdc44915507c0c81c426a7633cfd0eb13d
brunolralves <brunolralves@yahoo.com>
2021-11-03 19:02:37 -0300

    Ajustado calculo do quoeficiente de atributos

---
+++ CalculadoraDeSalarios/Classes/Player.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeSalarios.Classes
+{
+    internal class Player
+    {
+        public String Nome { get; set; }
+        public int Offense { get; set; }
+        public int Deffense { get; set; }
+        public int BodyBalance { get; set; }
+        public int Stamina { get; set; }
+        public int Speed { get; set; }
+        public int Acceleration { get; set; }
+        public int Pass { get; set; }
+        public int ShootPower { get; set; }
+        public int ShootAcc { get; set; }
+        public int Jump { get; set; }
+        public int Head { get; set; }
+        public int Technique { get; set; }
+        public int Dribble { get; set; }
+        public int Curve { get; set; }
+        public int Aggresive { get; set; }
+        public int Response { get; set; }
+        public Double PlayerSalary { get; set; }
+        public String PlayerClass { get; set; }
+        
+
+
+
+
+        public Player()
+        {
+
+        }
+
+        public Player(string nome, int offense, int deffense, int bodyBalance, int stamina, int speed, int acceleration, int pass, int shootPower, int shootAcc, int jump, int head, int technique, int dribble, int curve, int aggresive, int response)
+        {
+            Nome = nome;
+            Offense = offense;
+            Deffense = deffense;
+            BodyBalance = bodyBalance;
+            Stamina = stamina;
+            Speed = speed;
+            Acceleration = acceleration;
+            Pass = pass;
+            ShootPower = shootPower;
+            ShootAcc = shootAcc;
+            Jump = jump;
+            Head = head;
+            Technique = technique;
+            Dribble = dribble;
+            Curve = curve;
+            Aggresive = aggresive;
+            Response = response;
+        }
+
+        
+        public int CalcKey()
+        {
+            int key;
+
+            key = (Offense
+                  + Deffense
+                  + BodyBalance
+                  + Stamina
+                  + Speed
+                  + Acceleration
+                  + Pass
+                  + ShootPower
+                  + ShootAcc
+                  + Jump
+                  + Head
+                  + Technique
+                  + Dribble
+                  + Curve
+                  + Aggresive
+                  + Response);
+
+            return key;
+        }
+
+        int key;
+        public void CalcSalary(int playerClass)
+        {
+            double salary;
+            double tax = 0;
+            key = CalcKey();
+
+            switch (playerClass)
+            {
+                //Lendas = 2
+                case 1:
+                    tax = 2;
+                    PlayerClass = "Lendas";
+                    break;
+                //Euro = 1.5
+                case 2:
+                    tax = 1.5;
+                    PlayerClass = "Euro";
+                    break;
+                //Fora do eixo = 1.5
+                case 3:
+                    tax = 1.5;
+                    PlayerClass = "Fora do eixo";
+                    break;
+                // BR A = 1(Salario puro)
+                case 4:
+                    tax = 1;
+                    PlayerClass = "BR A";
+                    break;
+                // BR B = 0.8
+                case 5:
+                    tax = 0.8;
+                    PlayerClass = "BR B";
+                    break;
+                // BR C = 0.8
+                case 6:
+                    tax = 0.8;
+                    PlayerClass = "C";
+                    break;
+
+                default:
+                    break;
+            }
+
+            salary = (((key - 192) * 50) / 112) * tax;
+
+
+            PlayerSalary = salary;
+        }
+
+        public override string ToString()
+        {
+            return "Salario do Jogador "
+                  + Nome
+                  + ": "
+                  +PlayerSalary
+                  
+                  ;
+        }
+
+
+    }
+}
--- CalculadoraDeSalarios/Program.cs
+++ CalculadoraDeSalarios/Program.cs
@@ -1,4 +1,128 @@
 ﻿
-Console.WriteLine("Para calcularmos o salario iremos precisar de alguns dados!");
+using CalculadoraDeSalarios.Classes;
+
+
+
+List<Player> listPlayers = new List<Player>();
+
+
+Boolean keep = true;
+string option = " ";
+while (keep)
+{
+    Console.WriteLine("Para calcularmos o salario iremos precisar de alguns dados!");
+    Console.WriteLine("Informe o nome do jogador");
+    String nome = Console.ReadLine();
+
+    Console.WriteLine("------------------------------------------------------------");
+    Console.WriteLine("Agora vamos montar a arvore de atributos para definir o salario do jogador - " + nome);
+    Console.WriteLine("Cada um dos atributos de receber um valor entre 12 e 19 pontos!");
+    Console.WriteLine("Vamos la!");
+    Console.Write("Offense:");
+    int offense = int.Parse(Console.ReadLine());
+    Console.Write("Deffense:");
+    int deffense = int.Parse(Console.ReadLine());
+    Console.Write("BodyBalance:");
+    int bodyBalance = int.Parse(Console.ReadLine());
+    Console.Write("Stamina:");
+    int stamina = int.Parse(Console.ReadLine());
+    Console.Write("Speed:");
+    int speed = int.Parse(Console.ReadLine());
+    Console.Write("Acceleration:");
+    int acceleration = int.Parse(Console.ReadLine());
+    Console.Write("Pass:");
+    int pass = int.Parse(Console.ReadLine());
+    Console.Write("Shoot Power:");
+    int shootPower = int.Parse(Console.ReadLine());
+    Console.Write("Shoot Acc.:");
+    int shootAcc = int.Parse(Console.ReadLine());
+    Console.Write("Jump:");
+    int jump = int.Parse(Console.ReadLine());
+    Console.Write("Head:");
+    int head = int.Parse(Console.ReadLine());
+    Console.Write("Technique:");
+    int technique = int.Parse(Console.ReadLine());
+    Console.Write("Dribble:");
+    int dribble = int.Parse(Console.ReadLine());
+    Console.Write("Curve:");
+    int curve = int.Parse(Console.ReadLine());
+    Console.Write("Aggresive:");
+    int aggresive = int.Parse(Console.ReadLine());
+    Console.Write("Response:");
+    int response = int.Parse(Console.ReadLine());
+    Console.Clear();
+
+    Console.WriteLine();
+
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine("Agora preciso saber a qual classe pertence o jogador - " + nome);
+    Console.WriteLine();
+    Console.WriteLine("Pressione a opção equivalente a classe do jogador!");
+
+    int classNumber;
+    do
+    {
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("Agora preciso saber a qual classe pertence o jogador - " +nome);
+        Console.WriteLine();
+        Console.WriteLine("Pressione a opção equivalente a classe do jogador!");
+        Console.WriteLine();
+        Console.WriteLine("1 - Lenda ");
+        Console.WriteLine("2 - Euro ");
+        Console.WriteLine("3 - Fora do Eixo ");
+        Console.WriteLine("4 - BR - A ");
+        Console.WriteLine("5 - BR - B ");
+        Console.WriteLine("6 - BR - C ");
+
+        classNumber = int.Parse(Console.ReadLine());
+
+        if (classNumber < 1 || classNumber > 6)
+        {
+            Console.WriteLine("Valor inserido invalido, vamos tentar novamente!");
+            classNumber = 0;
+        }
+
+    } while (classNumber == 0);
+
+    Player newPlayer = new(nome,offense,deffense,bodyBalance,stamina,speed,acceleration,pass,shootPower,shootAcc,jump,head,technique,dribble,curve, aggresive, response);
+
+
+    newPlayer.CalcSalary(classNumber);
+
+
+
+
+    bool verifyCondition = false;
+    do
+    {
+        Console.WriteLine("Deseja inserir mais algum jogador? S/N");
+        option = Console.ReadLine().ToUpper();
+        if (option == "N")
+        {
+            keep = false;
+            verifyCondition = false;
+        }
+        else if (option == "S")
+        {
+            keep = true;
+            verifyCondition = false;
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine("Valor Invalido!!");
+            verifyCondition = true;
+        }
+
+    } while (verifyCondition);
+
+    Console.WriteLine(newPlayer);
+
+  
+}
+
+
+
+
 
 
