a6c75cacfb4e91b9b3babe80c4b4a410bb1efbd4
brunolralves <brunolralves@yahoo.com>
2021-11-07 15:49:33 -0300

    Incrementada exibição da classe a qual perrtence o jogador

--- CalculadoraDeSalarios/Classes/Player.cs
+++ CalculadoraDeSalarios/Classes/Player.cs
@@ -133,6 +133,8 @@
                   + Nome
                   + ": "
                   + PlayerSalary
+                  + " Classe: "
+                  + PlayerClass
 
                   ;
         }
